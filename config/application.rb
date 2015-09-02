631e13fccf45a6429c53b53909906d6697cc4083
tech-camp-047 <tech-camp-047@tech-camp-047-no-MacBook-Air.local>
2015-09-02 19:28:14 +0900

    初コミット

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ module BeautySite
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :je
+    # I18n.enforce_available_locales = true
+    config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
